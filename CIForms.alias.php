4e2ea7b62e8e084d2b8bcfb8a12084a981f4123f
anterdc99 <xuminruixuan@live.cn>
2025-07-14 11:25:33 +0800

    CIForms: Translate special page aliases into Chinese
    Change-Id: I5351c861dab523879a5655564c66e51ec0c65a30

--- CIForms.alias.php
+++ CIForms.alias.php
@@ -14,3 +14,27 @@ $specialPageAliases['en'] = [
 	'CIFormsSubmit' => [ 'CIFormsSubmit' ],
 	'CIFormsManage' => [ 'CIFormsManage' ],
 ];
+
+/**
+ * Chinese (中文)
+ */
+$specialPageAliases['zh'] = [
+	'CIFormsSubmit' => [ 'CIFormsSubmit' ],
+	'CIFormsManage' => [ 'CIFormsManage' ],
+];
+
+/**
+ * Simplified Chinese (中文（简体）)
+ */
+$specialPageAliases['zh-hans'] = [
+	'CIFormsSubmit' => [ '提交CI表单' ],
+	'CIFormsManage' => [ '管理CI表单' ],
+];
+
+/**
+ * Traditional Chinese (中文（繁體）)
+ */
+$specialPageAliases['zh-hant'] = [
+	'CIFormsSubmit' => [ '提交CI表單' ],
+	'CIFormsManage' => [ '管理CI表單' ],
+];
